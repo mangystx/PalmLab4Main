9306d48d966b9d0e1c009bc9a4c3175a4aace23f
IKVolod <120046495+IKVolod@users.noreply.github.com>
2023-04-09 20:20:55 +0300

    Update Ivan.cs

--- PalmLab4Main/Ivan.cs
+++ PalmLab4Main/Ivan.cs
@@ -3,7 +3,7 @@ namespace PalmLab4;
 public class Ivan
 {
     //Завдання №1
-    static int[] Find_Even_One(ref int[] mass)
+    static void Find_Even_One(ref int[] mass)
     {
         if (Is_Even_Exists(in mass, out int indexOfEvenNumber))
         {
@@ -16,7 +16,6 @@ public class Ivan
         {
             Console.WriteLine("\nНемає парних елементiв в масивi");
         }
-        return mass;
     }
     static bool Is_Even_Exists(in int[] mass, out int indexOfEvenNumber)
     {
@@ -44,13 +43,12 @@ public class Ivan
         Array.Resize(ref mass, n - 1);
     }
     //Завдання №2
-    static int[][] Extend_Mass(ref int[][] mass)
+    static void Extend_Mass(ref int[][] mass)
     {
         Array.Resize(ref mass, mass.Length + 1);
         Add_Row(ref mass, in BonusRow_Index());
         Console.WriteLine("/nРезультуючий масив");
         Print_Jagged_Mass(in mass);
-        return mass;
     }
     static int BonusRow_Index()
     {
@@ -60,11 +58,11 @@ public class Ivan
     }
     static void Add_Row(ref int[][] mass, in int index)
     {
-        Console.WriteLine("\nРядок, який додається, вписується");
+        Console.WriteLine("\nВпишiть рядок, який додається");
         mass[index] = Array.ConvertAll(Console.ReadLine().Trim().Split(), int.Parse);
     }
     //Завдання №3
-    static int[][] Change_ZeroElement_With_MinElement(ref int[][] mass)
+    static void Change_ZeroElement_With_MinElement(ref int[][] mass)
     {
         for (int i = 0; i < mass.Length; i++)
         {
@@ -82,13 +80,16 @@ public class Ivan
             mass[i][0] = mass[i][k];
             mass[i][k] = temp;
         }
-        Row_With_Max_Element(in int[][] mass, out int index)
+        Console.WriteLine("/nМасив, пiсля замiни елементiв");
+        Print_Jagged_Mass(in mass);
+        Row_With_Max_Element(in int[][] mass, out int index);
+        Console.WriteLine($"/nРядок з першим максимальним елементом |{index + 1}|");
+        PrintMassAsOneString(in mass[index]);
         Array.Reverse(mass[index]);
         Console.WriteLine("/nРеверсивний рядок");
         PrintMassAsOneString(in mass[index]);
         Console.WriteLine("/nРезультуючий масив");
         Print_Jagged_Mass(in mass);
-        return mass;
     }
     static void Row_With_Max_Element(in int[][] mass, out int index)
     {
